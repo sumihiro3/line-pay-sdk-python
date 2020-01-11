4d64c8d6558865a4362e8b326a60b1cc31979833
sumihiro3 <sumihiro@gmail.com>
2020-01-11 17:09:24 +0900

    modify setup and readme for register to PyPI

--- README.rst
+++ README.rst
@@ -1,7 +1,6 @@
 LINE Pay API SDK for Python
 =================================
 
-|Build Status| |PyPI version| |Documentation Status|
 
 SDK of the LINE Pay API for Python.
 
@@ -10,8 +9,8 @@ Introduction
 The LINE Pay API SDK for Python makes it easy to develop apps using LINE Pay API.
 
 
-Documentation
--------------
+API Documentation
+-----------------
 
 See the official API documentation for more information
 
@@ -24,3 +23,20 @@ Requirements
 
 -  Python >= 3.6
 
+Installation
+------------
+
+::
+
+    $ pip install line-pay
+
+Hints
+-----
+
+Examples
+~~~~~~~~
+
+`Example usage <https://github.com/sumihiro3/line-pay-sdk-python/tree/master/examples>`__
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+Example using `Flask <http://flask.pocoo.org/>`__
--- setup.py
+++ setup.py
@@ -1,21 +1,31 @@
 from setuptools import setup, find_packages
 
 def _requirements():
-    with open('requirements.txt', 'r') as fd:
+    with open("requirements.txt", "r") as fd:
         return [name.strip() for name in fd.readlines()]
 
-with open('README.rst', 'r') as fd:
+with open("README.rst", "r") as fd:
     long_description = fd.read()
 
 setup(
-    name='line-pay',
-    version='0.0.1',
-    description='LINE Pay API SDK for Python',
+    name="line-pay",
+    version="0.0.1",
+    description="LINE Pay API SDK for Python",
     long_description=long_description,
-    author='Sumihiro Kagawa',
-    author_email='sumihiro@gmail.com',
-    url='https://github.com/sumihiro3/line-pay-sdk-python',
+    long_description_content_type="text/x-rst",
+    author="Sumihiro Kagawa",
+    author_email="sumihiro@gmail.com",
+    url="https://github.com/sumihiro3/line-pay-sdk-python",
     license=license,
-    packages=find_packages(exclude=('tests', 'docs', 'requests', 'examples')),
+    packages=find_packages(exclude=("tests", "docs", "requests", "examples")),
 	install_requires=_requirements(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development"
+    ],
+    keywords=["LINE", "LINE Pay"]
 )
